9d47757343aee6a42236f0bf8a7adf568f79e824
Ваше Имя <you@example.com>
2025-03-06 03:10:36 +0700

    lab4

--- lab4/server.cpp
+++ lab4/server.cpp
@@ -1,82 +1,133 @@
-#include <iostream>
-#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <errno.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
 
-#define PORT 0 // Автоматический выбор свободного порта
-#define BUFFER_SIZE 1024
+#define BACKLOG 5
+#define BUFFER_SIZE 256
 
-int main()
+int main(void)
 {
-  int sockfd;
+  int msock, newfd, maxfd;
   struct sockaddr_in server_addr, client_addr;
-  socklen_t addr_len = sizeof(client_addr);
-  char buffer[BUFFER_SIZE];
-  int port;
+  socklen_t addrlen;
+  fd_set master_set, read_set;
 
-  // Создание UDP сокета
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+  msock = socket(AF_INET, SOCK_STREAM, 0);
+  if (msock < 0)
   {
-    std::cerr << "Socket creation failed: " << strerror(errno) << std::endl;
-    return 1;
+    perror("socket error");
+    exit(EXIT_FAILURE);
   }
-  std::cout << "Socket created successfully" << std::endl;
 
-  // Настройка адреса сервера
   memset(&server_addr, 0, sizeof(server_addr));
   server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(PORT);
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port = 0;
 
-  // Привязка сокета
-  if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+  if (bind(msock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
   {
-    std::cerr << "Bind failed: " << strerror(errno) << std::endl;
-    return 1;
+    perror("bind error");
+    close(msock);
+    exit(EXIT_FAILURE);
   }
-  std::cout << "Bind successful" << std::endl;
 
-  // Получение назначенного порта
-  addr_len = sizeof(server_addr);
-  if (getsockname(sockfd, (struct sockaddr *)&server_addr, &addr_len) < 0)
+  addrlen = sizeof(server_addr);
+  if (getsockname(msock, (struct sockaddr *)&server_addr, &addrlen) < 0)
   {
-    std::cerr << "Getsockname failed: " << strerror(errno) << std::endl;
-    return 1;
+    perror("getsockname error");
+    close(msock);
+    exit(EXIT_FAILURE);
   }
-  port = ntohs(server_addr.sin_port);
-  std::cout << "Server running on port: " << port << std::endl;
-  std::cout.flush(); // Принудительный сброс буфера
+  printf("Сервер слушает на порту: %d\n", ntohs(server_addr.sin_port));
 
-  while (true)
+  // Переводим сокет в режим прослушивания
+  if (listen(msock, BACKLOG) < 0)
   {
-    // Прием данных от клиента
-    int n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
-                     (struct sockaddr *)&client_addr, &addr_len);
-    if (n < 0)
+    perror("listen error");
+    close(msock);
+    exit(EXIT_FAILURE);
+  }
+
+  // Инициализируем множество дескрипторов и добавляем слушающий сокет
+  FD_ZERO(&master_set);
+  FD_SET(msock, &master_set);
+  maxfd = msock;
+
+  // Открываем общий файл для логирования сообщений (режим добавления)
+  FILE *logfile = fopen("data.txt", "a");
+  if (!logfile)
+  {
+    perror("Ошибка открытия файла");
+    close(msock);
+    exit(EXIT_FAILURE);
+  }
+
+  while (1)
+  {
+    read_set = master_set; // делаем копию множества дескрипторов для select
+    int activity = select(maxfd + 1, &read_set, NULL, NULL, NULL);
+    if (activity < 0)
     {
-      std::cerr << "Receive failed: " << strerror(errno) << std::endl;
-      continue;
+      perror("select error");
+      break;
     }
-    buffer[n] = '\0';
 
-    // Вывод информации о клиенте и полученных данных
-    char client_ip[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
-    std::cout << "Received from client " << client_ip << ":"
-              << ntohs(client_addr.sin_port) << ": " << buffer << std::endl;
-    std::cout.flush();
-
-    // Преобразование строки (добавление "[SERVER]" в конец)
-    strcat(buffer, " [SERVER]");
-
-    // Отправка преобразованных данных обратно клиенту
-    sendto(sockfd, buffer, strlen(buffer), 0,
-           (struct sockaddr *)&client_addr, addr_len);
+    // Проходим по всем дескрипторам от 0 до maxfd
+    for (int fd = 0; fd <= maxfd; fd++)
+    {
+      if (FD_ISSET(fd, &read_set))
+      {
+        if (fd == msock)
+        {
+          // Новое подключение
+          addrlen = sizeof(client_addr);
+          newfd = accept(msock, (struct sockaddr *)&client_addr, &addrlen);
+          if (newfd < 0)
+          {
+            perror("accept error");
+            continue;
+          }
+          FD_SET(newfd, &master_set);
+          if (newfd > maxfd)
+            maxfd = newfd;
+          printf("Новое подключение: %s, сокет %d\n",
+                 inet_ntoa(client_addr.sin_addr), newfd);
+        }
+        else
+        {
+          // Данные от существующего клиента
+          int number;
+          int n = read(fd, &number, sizeof(number));
+          if (n <= 0)
+          {
+            if (n == 0)
+              printf("Сокет %d: соединение закрыто клиентом\n", fd);
+            else
+              perror("read error");
+            close(fd);
+            FD_CLR(fd, &master_set);
+          }
+          else
+          {
+            // Вывод полученного числа на экран
+            printf("Получено число: %d от сокета %d\n", number, fd);
+            // Запись в лог-файл
+            fprintf(logfile, "Сокет %d: %d\n", fd, number);
+            fflush(logfile);
+          }
+        }
+      }
+    }
   }
 
-  close(sockfd);
+  fclose(logfile);
+  close(msock);
   return 0;
 }
